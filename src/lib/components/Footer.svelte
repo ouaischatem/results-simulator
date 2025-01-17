d50d43fa9216a2df17db93ef40f5dabd538d2625
Hatem <obeidinehatem99@gmail.com>
2025-01-18 00:17:38 +0100

    :construction: Add simple header/footer components

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="text-center text-lg my-5">
+	Développé par <a class="underline" href="https://callhatem.fr/">Hatem OBEIDINE</a> 🔥
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,3 @@
+<div class="bg-primary p-5 drop-shadow-2xl">
+	<h1 class="text-white font-bold text-3xl">Simulateur de relevé de notes</h1>
+</div>
